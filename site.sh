3a50cd8a750e21d4410c572586722f12089587b5
mvcardim <mvcardim@gmail.com>
2022-11-22 10:57:25 -0300

    ultima atualizaçao

--- site.sh
+++ site.sh
@@ -1,5 +1,5 @@
-sudo apt install docker-compose -y
-sudo chmod +x /usr/local/bin/docker-compose
-git clone  https://github.com/mvcardim/ApWeb_Docker.git
-cd /home/vagrant/ApWeb_Docker
-docker-compose up -d
+mkdir site
+cd site
+docker pull mvcardim/my-apache2:v1
+docker run -dit --name my-appApache -p 80:80 mvcardim/my-apache2:v1
+
